572c1e23dd32765fb63a9766425d99821ffb5b9b
Gwladys Engels <gwladys.engels@gmail.com>
2022-05-25 15:05:29 -0400

    removed duplicate migration

--- db/migrate/20220525154802_add_coordinates_to_restaurants.rb
+++
@@ -1,6 +0,0 @@
-class AddCoordinatesToRestaurants < ActiveRecord::Migration[6.1]
-  def change
-    add_column :restaurants, :latitude, :float
-    add_column :restaurants, :longitude, :float
-  end
-end
