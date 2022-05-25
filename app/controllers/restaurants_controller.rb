18e000819355282bcc179a14161c974312051af5
Reem Gawad <reemabdelg@gmail.com>
2022-05-25 11:28:59 -0400

    added favoritor gem, added actions in restaurants_controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,6 +9,14 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
     @visit = Visit.new
     @visits = @restaurant.visits
+  end
 
+  def toggle_favorite
+    @restaurant = Restaurant.find(params[:id])
+    current_user.favorited?(@restaurant) ? current_user.unfavorite(@restaurant) : current_user.favorite(@restaurant)
+  end
+
+  def my_favorites
+    @favorites = current_user.all_favorites
   end
 end
