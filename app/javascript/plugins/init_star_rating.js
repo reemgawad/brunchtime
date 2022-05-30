8ebd77de2884572abc7076b6149dc0d101d10a3d
Lien Nguyen <lienhnguyen@outlook.com>
2022-05-30 17:49:36 -0400

    load error with stars

--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -1,11 +1,9 @@
-import "jquery-bar-rating";
-
 // app/javascript/plugins/init_star_rating.js
 import "jquery-bar-rating";
 import $ from 'jquery'; // <-- if you're NOT using a Le Wagon template (cf jQuery section)
 
 const initStarRating = () => {
-  // TODO
+  //TODO
   $('#visit_rating').barrating({
     theme: 'css-stars'
 
