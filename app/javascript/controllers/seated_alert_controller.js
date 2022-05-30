304f12d43c801d159307e3a1eef808f305954a3d
Reem Gawad <reemabdelg@gmail.com>
2022-05-30 10:05:02 -0400

    commented out consolelogs

--- app/javascript/controllers/seated_alert_controller.js
+++ app/javascript/controllers/seated_alert_controller.js
@@ -5,8 +5,8 @@ export default class extends Controller {
   static targets = ["myModal", "noButton"]
 
   connect() {
-    console.log(this.myModalTarget)
-    console.log(this.noButtonTarget)
+    // console.log(this.myModalTarget)
+    // console.log(this.noButtonTarget)
     setTimeout(() => {
       this.myModalTarget.click()
     }, 7000)
