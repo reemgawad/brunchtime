4d501634d1e6f498bf145b32efdc1282531caf79
Reem Gawad <reemabdelg@gmail.com>
2022-05-30 15:50:01 -0400

    trying to center alert

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -21,7 +21,7 @@ class VisitsController < ApplicationController
     # @visit.user = current_user
 
     if @visit.update(visit_params)
-      flash[:alert] = "Thank you!"
+      flash[:notice] = "Thank you!"
       redirect_to restaurant_path(@restaurant)
     else
       render :edit
