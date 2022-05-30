f4da2038618ae7e38c5af60ef009b3beaece57e1
Gwladys Engels <gwladys.engels@gmail.com>
2022-05-30 17:41:24 -0400

    fixed logos colors

--- app/javascript/controllers/toggle_favorite_controller.js
+++ app/javascript/controllers/toggle_favorite_controller.js
@@ -2,11 +2,12 @@ import { Controller } from "stimulus"
 
 
 export default class extends Controller {
-    static targets = ["output", "svgPath"]
+    static targets = ["output", "svgPath", "svgPath2"]
 
     connect() {}
 
     toggle(event) {
         this.svgPathTarget.classList.toggle('liked')
+        this.svgPath2Target.classList.toggle('liked')
     }
 }
