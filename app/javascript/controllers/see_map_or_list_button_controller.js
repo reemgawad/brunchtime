0dde2d5bbb0a931255d8915edc18dffae39d44ea
Reem Gawad <reemabdelg@gmail.com>
2022-05-31 11:23:36 -0400

    edited shadow + made margin smaller

--- app/javascript/controllers/see_map_or_list_button_controller.js
+++ app/javascript/controllers/see_map_or_list_button_controller.js
@@ -8,7 +8,7 @@ export default class extends Controller {
   show(event) {
     this.mapTarget.classList.toggle("d-none")
     this.listTarget.classList.toggle("d-none")
-    console.log(event.target.innerHTML);
+    // console.log(event.target.innerHTML);
     if (event.target.innerHTML == 'See Map <i class="far fa-map"></i>') {
       event.target.innerHTML = 'See List <i class="fas fa-list-ul"></i>'
     } else if (event.target.innerHTML == 'See List <i class="fas fa-list-ul"></i>') {
