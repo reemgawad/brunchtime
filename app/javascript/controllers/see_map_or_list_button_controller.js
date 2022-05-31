4bd14a833a04a60e5336fc4082b1da54f1d27b71
Reem Gawad <reemabdelg@gmail.com>
2022-05-31 10:29:15 -0400

    added map/list icons + toggle between them

--- app/javascript/controllers/see_map_or_list_button_controller.js
+++ app/javascript/controllers/see_map_or_list_button_controller.js
@@ -10,10 +10,9 @@ export default class extends Controller {
     this.listTarget.classList.toggle("d-none")
     console.log(event.target.innerHTML);
     if (event.target.innerHTML == 'See Map <i class="far fa-map"></i>') {
-      event.target.innerHTML = "See List"
-    } else if (event.target.innerText == "See List") {
-      event.target.innerText = "See Map"
+      event.target.innerHTML = 'See List <i class="fas fa-list-ul"></i>'
+    } else if (event.target.innerHTML == 'See List <i class="fas fa-list-ul"></i>') {
+      event.target.innerHTML = 'See Map <i class="far fa-map"></i>'
     }
-
   }
 }
