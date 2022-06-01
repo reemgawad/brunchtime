d9ab001b6a00d4e0b053c4ac306df17913a4000c
Gwladys Engels <gwladys.engels@gmail.com>
2022-06-01 10:14:49 -0400

    added ratings for 3 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,7 +45,73 @@ user_four = User.create!(
   }
 )
 
+user_five = User.create!(
+  {
+    email: "laura@email.com",
+    first_name: "Laura",
+    password: "secret"
+  }
+)
+
+user_six = User.create!(
+  {
+    email: "paul@email.com",
+    first_name: "Paul",
+    password: "secret"
+  }
+)
+
+user_seven = User.create!(
+  {
+    email: "sam@email.com",
+    first_name: "Sam",
+    password: "secret"
+  }
+)
+
+user_eight = User.create!(
+  {
+    email: "billy@email.com",
+    first_name: "Billy",
+    password: "secret"
+  }
+)
+
+user_nine = User.create!(
+  {
+    email: "chloe@email.com",
+    first_name: "Chloe",
+    password: "secret"
+  }
+)
+
+user_ten = User.create!(
+  {
+    email: "louis@email.com",
+    first_name: "Louis",
+    password: "secret"
+  }
+)
+
+user_eleven = User.create!(
+  {
+    email: "bob@email.com",
+    first_name: "Bob",
+    password: "secret"
+  }
+)
+
+user_twelve = User.create!(
+  {
+    email: "marie@email.com",
+    first_name: "Marie",
+    password: "secret"
+  }
+)
+
 print "Seeding restaurants..."
+
+
 # Restaurants
 restaurant_one = Restaurant.new(
   {
@@ -200,7 +266,9 @@ puts "Done."
 print "Seeding visits..."
 
 # Visits
-visit_one = Visit.new(
+# resto1
+
+visit_onea = Visit.new(
   {
     date: Date.today - 5,
     feedback: "Waited 12 min. Wait-time close to accurate.",
@@ -208,11 +276,147 @@ visit_one = Visit.new(
     arrived: true
   }
 )
-visit_one.user = user_four
-visit_one.restaurant = restaurant_one
-visit_one.save!
+visit_onea.user = user_four
+visit_onea.restaurant = restaurant_one
+visit_onea.save!
 
-visit_two = Visit.new(
+visit_oneb = Visit.new(
+  {
+    date: Date.today - 3,
+    feedback: "Wait-time was a bit longer than announced. (still worth it !)",
+    rating: 3,
+    arrived: true
+  }
+)
+visit_oneb.user = user_eight
+visit_oneb.restaurant = restaurant_one
+visit_oneb.save!
+
+visit_onec = Visit.new(
+  {
+    date: Date.today - 11,
+    feedback: "Wait time was showing 30 min and we got a table in 35min ! awesome",
+    rating: 5,
+    arrived: true
+  }
+)
+visit_onec.user = user_seven
+visit_onec.restaurant = restaurant_one
+visit_onec.save!
+
+visit_oned = Visit.new(
+  {
+    date: Date.today - 20,
+    feedback: "Wait time was fairly accurate !",
+    rating: 4,
+    arrived: true
+  }
+)
+visit_oned.user = user_six
+visit_oned.restaurant = restaurant_one
+visit_oned.save!
+
+visit_onee = Visit.new(
+  {
+    date: Date.today - 36,
+    feedback: "We unfortunately had to wait 20min longer than expected to get a table!",
+    rating: 2,
+    arrived: true
+  }
+)
+visit_onee.user = user_eleven
+visit_onee.restaurant = restaurant_one
+visit_onee.save!
+
+# resto2
+visit_foura = Visit.new(
+  {
+    date: Date.today - 14,
+    feedback: "20min wait not 10. Was double what I expected.",
+    rating: 1,
+    arrived: true
+  }
+)
+visit_foura.user = user_four
+visit_foura.restaurant = restaurant_two
+visit_foura.save!
+
+visit_fourb = Visit.new(
+  {
+    date: Date.today - 3,
+    feedback: "Got a table within expected time , thanks brunchtime!",
+    rating: 4,
+    arrived: true
+  }
+)
+visit_fourb.user = user_six
+visit_fourb.restaurant = restaurant_two
+visit_fourb.save!
+
+visit_fourc = Visit.new(
+  {
+    date: Date.today - 10,
+    feedback: "Accuracy was pretty good; we had to wait an extra 15min - not so bad ",
+    rating: 3,
+    arrived: true
+  }
+)
+visit_fourc.user = user_ten
+visit_fourc.restaurant = restaurant_two
+visit_fourc.save!
+
+visit_fourd = Visit.new(
+  {
+    date: Date.today - 30,
+    feedback: "Busy as usual but waiting time was accurately displayed",
+    rating: 3,
+    arrived: true
+  }
+)
+visit_fourd.user = user_twelve
+visit_fourd.restaurant = restaurant_two
+visit_fourd.save!
+
+visit_foure = Visit.new(
+  {
+    date: Date.today - 3,
+    feedback: "The accuracy was off ; we showed up hoping to wait for 45min but ended up waiting 20min extra to get a table",
+    rating: 1,
+    arrived: true
+  }
+)
+visit_foure.user = user_eight
+visit_foure.restaurant = restaurant_two
+visit_foure.save!
+
+visit_fourf = Visit.new(
+  {
+    date: Date.today - 40,
+    feedback: "Great time accuracy",
+    rating: 5,
+    arrived: true
+  }
+)
+visit_fourf.user = user_three
+visit_fourf.restaurant = restaurant_two
+visit_fourf.save!
+
+visit_fourg = Visit.new(
+  {
+    date: Date.today - 7,
+    feedback: "Fair accuracy; only xaited an extra 10min to get seated",
+    rating: 4,
+    arrived: true
+  }
+)
+visit_fourg.user = user_seven
+visit_fourg.restaurant = restaurant_two
+visit_fourg.save!
+
+
+# resto3
+
+visit_twoa = Visit.new(
   {
     date: Date.today - 30,
     feedback: "Waited 10 min. Faster than expected wait-time :)",
@@ -220,9 +424,59 @@ visit_two = Visit.new(
     arrived: true
   }
 )
-visit_two.user = user_three
-visit_two.restaurant = restaurant_three
-visit_two.save!
+visit_twoa.user = user_three
+visit_twoa.restaurant = restaurant_three
+visit_twoa.save!
+
+visit_twob = Visit.new(
+  {
+    date: Date.today - 10,
+    feedback: "spot on !",
+    rating: 5,
+    arrived: true
+  }
+)
+visit_twob.user = user_seven
+visit_twob.restaurant = restaurant_three
+visit_twob.save!
+
+visit_twoc = Visit.new(
+  {
+    date: Date.today - 6,
+    feedback: "Waited 1h to be seated but it was showing 40min",
+    rating: 3,
+    arrived: true
+  }
+)
+visit_twoc.user = user_eleven
+visit_twoc.restaurant = restaurant_three
+visit_twoc.save!
+
+visit_twod = Visit.new(
+  {
+    date: Date.today - 15,
+    feedback: "Waiting time was as expected",
+    rating: 4,
+    arrived: true
+  }
+)
+visit_twod.user = user_ten
+visit_twod.restaurant = restaurant_three
+visit_twod.save!
+
+visit_twoe = Visit.new(
+  {
+    date: Date.today - 27,
+    feedback: "Great accuracy ! so helpfully to find the best places without bad surprises ",
+    rating: 4,
+    arrived: true
+  }
+)
+visit_twoe.user = user_nine
+visit_twoe.restaurant = restaurant_three
+visit_twoe.save!
+
+# resto4
 
 visit_three = Visit.new(
   {
@@ -236,18 +490,7 @@ visit_three.user = user_two
 visit_three.restaurant = restaurant_four
 visit_three.save!
 
-visit_four = Visit.new(
-  {
-    date: Date.today - 14,
-    feedback: "20min wait not 10. Was double what I expected.",
-    rating: 1,
-    arrived: true
-  }
-)
-visit_four.user = user_four
-visit_four.restaurant = restaurant_two
-visit_four.save!
-puts "Done."
+# resto5
 
 visit_five = Visit.new(
   {
@@ -261,17 +504,7 @@ visit_five.user = user_three
 visit_five.restaurant = restaurant_five
 visit_five.save!
 
-visit_six = Visit.new(
-  {
-    date: Date.today - 11,
-    feedback: "Waited exactly 15 min!",
-    rating: 5,
-    arrived: true
-  }
-)
-visit_six.user = user_two
-visit_six.restaurant = restaurant_ten
-visit_six.save!
+# resto6
 
 visit_seven = Visit.new(
   {
@@ -285,6 +518,8 @@ visit_seven.user = user_four
 visit_seven.restaurant = restaurant_six
 visit_seven.save!
 
+# resto7
+
 visit_eight = Visit.new(
   {
     date: Date.today - 4,
@@ -296,6 +531,8 @@ visit_eight = Visit.new(
 visit_eight.user = user_three
 visit_eight.restaurant = restaurant_seven
 visit_eight.save!
+
+# resto8
 
 visit_nine = Visit.new(
   {
@@ -309,6 +546,8 @@ visit_nine.user = user_two
 visit_nine.restaurant = restaurant_eight
 visit_nine.save!
 
+# resto9
+
 visit_ten = Visit.new(
   {
     date: Date.today - 3,
@@ -321,12 +560,25 @@ visit_ten.user = user_one
 visit_ten.restaurant = restaurant_nine
 visit_ten.save!
 
+# resto10
+
+visit_six = Visit.new(
+  {
+    date: Date.today - 11,
+    feedback: "Waited exactly 15 min!",
+    rating: 5,
+    arrived: true
+  }
+)
+visit_six.user = user_two
+visit_six.restaurant = restaurant_ten
+visit_six.save!
 
 # restaurant_eleven = Restaurant.new(
 #   {
-#     name: "Ginko Café & BarTEST",
-#     address: "308 RUE STE-CATHERINE EST, MONTREAL",
-#     description: "Popular brunch spot serving eclectic Canadian fare in a chic, open room with lots of natural light.",
+  #     name: "Ginko Café & BarTEST",
+  #     address: "308 RUE STE-CATHERINE EST, MONTREAL",
+  #     description: "Popular brunch spot serving eclectic Canadian fare in a chic, open room with lots of natural light.",
 #     price_range: "$",
 #     opening_hours: "7am-9pm",
 #     phone_number: "514-379-4449",
