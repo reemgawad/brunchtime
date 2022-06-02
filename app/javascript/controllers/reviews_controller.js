83ceb6006f1dab25374320499b42b49ae2a7f6a4
Reem Gawad <reemabdelg@gmail.com>
2022-06-02 16:29:03 -0400

    made see reviews/hide reviews work if clicking on whole button

--- app/javascript/controllers/reviews_controller.js
+++ app/javascript/controllers/reviews_controller.js
@@ -1,21 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ["reviewsList"]
+    static targets = ["reviewsList", "hideList"]
 
       dropdown() {
           // console.log(this.reviewsListTarget.innerHTML)
           this.reviewsListTarget.classList.toggle('d-none')
+          if (this.hideListTarget.innerText == "See Reviews") {
+            this.hideListTarget.innerText = "Hide Reviews"
+          } else if (this.hideListTarget.innerText == "Hide Reviews") {
+            this.hideListTarget.innerText = "See Reviews"
+          }
       }
       connect() {
         // console.log("Hi")
       }
-      show(event) {
-        console.log(event.target.innerText)
-        if (event.target.innerText == "See Reviews") {
-          event.target.innerText = "Hide Reviews"
-        } else if (event.target.innerText == "Hide Reviews") {
-          event.target.innerText = "See Reviews"
-        }
-      }
+      // show(event) {
+      //   console.log(event.target.innerText)
+      //   if (event.target.innerText == "See Reviews") {
+      //     event.target.innerText = "Hide Reviews"
+      //   } else if (event.target.innerText == "Hide Reviews") {
+      //     event.target.innerText = "See Reviews"
+      //   }
+      // }
     }
