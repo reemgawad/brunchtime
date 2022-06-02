0fed11633467ddc6655cd395aabc33f817235e6c
Lien Nguyen <lienhnguyen@outlook.com>
2022-06-02 13:42:51 -0400

    re-did see map with z-index and removed d-none toggle

--- app/javascript/controllers/see_map_or_list_button_controller.js
+++ app/javascript/controllers/see_map_or_list_button_controller.js
@@ -6,13 +6,17 @@ export default class extends Controller {
   connect() {}
 
   show(event) {
-    this.mapTarget.classList.toggle("d-none")
-    this.listTarget.classList.toggle("d-none")
-    // console.log(event.target.innerHTML);
+    // this.mapTarget.classList.toggle("d-none")
+    // this.listTarget.classList.toggle("d-none")
+
     if (event.target.innerHTML == 'See Map <i class="far fa-map"></i>') {
       event.target.innerHTML = 'See List <i class="fas fa-list-ul"></i>'
+      this.mapTarget.style.zIndex = '400'
+      this.listTarget.style.zIndex = '100'
     } else if (event.target.innerHTML == 'See List <i class="fas fa-list-ul"></i>') {
       event.target.innerHTML = 'See Map <i class="far fa-map"></i>'
+      this.mapTarget.style.zIndex = '100'
+      this.listTarget.style.zIndex = '400'
     }
   }
 }
